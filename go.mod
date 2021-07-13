9893272b5f18a64a858438acf6999a19cd4836c6
Cloudio Wu <cloudio.wu@gmail.com>
2021-07-13 15:08:08 +0800

    完成 有點問題

--- go.mod
+++ go.mod
@@ -3,8 +3,7 @@ module chungcheng/chatroom
 go 1.16
 
 require (
-	github.com/gin-gonic/gin v1.7.2 // indirect
-	github.com/googollee/go-socket.io v1.6.0 // indirect
-	github.com/labstack/echo v3.3.10+incompatible // indirect
-	github.com/labstack/gommon v0.3.0 // indirect
+	github.com/gin-gonic/gin v1.7.2
+	github.com/gorilla/websocket v1.4.2
+	github.com/stretchr/testify v1.5.1 // indirect
 )
--- main.go
+++ main.go
@@ -1,64 +1,148 @@
 package main
 
 import (
-	viewmodels "chungcheng/chatroom/src/viewmodel"
 	"fmt"
+	"html/template"
 	"log"
 	"net/http"
+	"time"
 
-	"github.com/gin-gonic/gin"
-	socketio "github.com/googollee/go-socket.io"
+	"github.com/gorilla/websocket"
 )
 
-func main() {
-	var conw []socketio.Conn = make([]socketio.Conn, 0)
-	fmt.Println("process start")
-
-	server := socketio.NewServer(nil)
-	//socket.OnEvent("ssdss", "", func() {})
-	server.OnConnect("connection", func(conn socketio.Conn) (err error) {
-		conn.SetContext("")
-		conw = append(conw, conn)
-		fmt.Println("connected:", conn.ID())
-		return nil
-	})
-
-	server.OnEvent("/", "notice", func(s socketio.Conn, msg string) {
-		fmt.Println("notice:", msg)
-		for _, val := range conw {
-			val.Emit("reply", msg)
-		}
-	})
-	server.OnEvent("/chat", "msg", func(s socketio.Conn, msg string) string {
-		s.SetContext(msg)
-		return "recv " + msg
-	})
-	server.OnEvent("/", "msg", func(s socketio.Conn, msg string) string {
-		s.SetContext(msg)
-		return "recv " + msg
-	})
-	server.OnEvent("/", "bye", func(s socketio.Conn) string {
-		last := s.Context().(string)
-		s.Emit("bye", last)
-		s.Close()
-		return last
-	})
-	go server.Serve()
-	defer server.Close()
-
-	router := gin.Default()
-	router.LoadHTMLGlob("template/*")
-	router.GET("/", handlerHtml)
-	router.GET("socket.io", gin.WrapH(server))
-	router.POST("socket.io", func(contex *gin.Context) {
-		server.ServeHTTP(contex.Writer, contex.Request)
-	})
-	log.Fatal(router.Run(":5000"))
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
 }
 
-func handlerHtml(c *gin.Context) {
-	data := new(viewmodels.IndexData)
-	data.Title = "首頁 hi"
-	data.Content = "我的第一個首頁"
-	c.HTML(http.StatusOK, "index.html", data)
+type hub struct {
+	clients map[*client]bool
+	borad   chan []byte
+	join    chan *client
+	leave   chan *client
+}
+
+//回傳指標、整個app共用
+func New() *hub {
+	return &hub{
+		clients: make(map[*client]bool),
+		borad:   make(chan []byte),
+		join:    make(chan *client),
+		leave:   make(chan *client),
+	}
+}
+
+func (h *hub) run() {
+	for {
+		select {
+		case client := <-h.join:
+			fmt.Println("join")
+			h.clients[client] = true
+			fmt.Println("h.clients len = ", len(h.clients))
+		case client := <-h.leave:
+			fmt.Println("leave")
+			if _, ok := h.clients[client]; ok {
+				delete(h.clients, client)
+				close(client.send)
+			}
+		case msg := <-h.borad:
+			fmt.Println("message = ", msg, ", count = ", len(h.clients))
+			for client := range h.clients {
+				select {
+				case client.send <- msg:
+				default:
+					fmt.Println("hub clients run default")
+					//delete(h.clients, client)
+					//close(client.send)
+				}
+			}
+		default:
+
+		}
+	}
+}
+
+type client struct {
+	conn *websocket.Conn
+	hub  *hub
+	send chan []byte
+}
+
+func (c *client) read() {
+	defer func() {
+		c.conn.Close()
+		c.hub.leave <- c
+	}()
+	fmt.Println("client start read")
+	c.conn.SetReadLimit(512)
+	c.conn.SetReadDeadline(time.Now().Add(60 * time.Second))
+	c.conn.SetPongHandler(func(string) error { c.conn.SetReadDeadline(time.Now().Add(60 * time.Second)); return nil })
+
+	for {
+		_, message, err := c.conn.ReadMessage()
+		if err != nil {
+			if websocket.IsUnexpectedCloseError(err, websocket.CloseGoingAway, websocket.CloseAbnormalClosure) {
+				log.Printf("error: %v", err)
+			}
+			//function終止了
+			break
+		}
+		fmt.Println("write to borad = ", string(message))
+		c.hub.borad <- message
+	}
+}
+
+func (c *client) write() {
+	ticker := time.NewTicker((60 * time.Second * 9) / 10)
+	fmt.Println("client start write")
+	for {
+		select {
+		case msg := <-c.hub.borad:
+			fmt.Println("write=", string(msg))
+			w, err := c.conn.NextWriter(websocket.TextMessage)
+			if err != nil {
+				return
+			}
+			w.Write(msg)
+			w.Close() //馬上寫出去
+		case <-ticker.C:
+			fmt.Println("ticker 延長")
+			c.conn.SetWriteDeadline(time.Now().Add(10 * time.Second))
+			if err := c.conn.WriteMessage(websocket.PingMessage, nil); err != nil {
+				return
+			}
+		}
+	}
+}
+
+func main() {
+	upgrader.CheckOrigin = func(r *http.Request) bool { return true }
+	h := New()
+	go h.run()
+	http.HandleFunc("/ws", func(rw http.ResponseWriter, r *http.Request) {
+		fmt.Println("ws start")
+		conn, err := upgrader.Upgrade(rw, r, nil)
+		if err != nil {
+			fmt.Println("error")
+			log.Fatal(err)
+		}
+		client := &client{conn: conn, hub: h}
+		client.hub.join <- client
+		go client.read()
+		go client.write()
+
+	})
+
+	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
+		temp, err := template.ParseFiles("./index.html")
+		if err != nil {
+			log.Fatal(err)
+		}
+		temp.Execute(rw, nil)
+	})
+	fmt.Println("start listen 5050")
+	if err := http.ListenAndServe(":5050", nil); err != nil {
+		log.Fatal(err)
+	}
+
 }
